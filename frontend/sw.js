5454d3837e7a8bbed947b77c3754f045f89e142e
tmart <astrovirenspacecow@gmail.com>
2026-01-10 14:16:05 -0600

    Initial backend deploy

---
+++ frontend/sw.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', e => {
+    console.log('Service Worker: Installed');
+});
+
+self.addEventListener('activate', e => {
+    console.log('Service Worker: Activated');
+});
+
+self.addEventListener('fetch', e => {
+    // Optional: add caching here
+    // e.respondWith(fetch(e.request));
+});
